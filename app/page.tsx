ea0ca72ca2360e1b20649942d670cfa6ce318302
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-11-13 21:34:57 +0400

    feature/dinamic card

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,64 @@
 'use client';
-import { useState, useEffect } from 'react';
-import Collection from '@/components/Collection';
+import { useEffect, useState } from 'react';
+import Collection from '../components/Collection';
 
-type collectionData = {
+type CollectionData = {
   category: number;
   name: string;
   photos: string[];
 };
 
 export default function Home() {
-  const [collection, setCollection] = useState<collectionData[]>([]);
+  const [collections, setCollections] = useState<CollectionData[]>([]);
+
+  useEffect(() => {
+    const fetchCollections = async () => {
+      try {
+        const response = await fetch(
+          'https://6734d2605995834c8a90ffac.mockapi.io/photos'
+        );
+        const data: CollectionData[] = await response.json();
+        setCollections(data);
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchCollections();
+  }, []);
+
   return (
     <div>
       <div>
-        <h1>my collection</h1>
+        <h1>My Colection</h1>
         <ul>
-          <li>home</li>
-          <li>test</li>
-          <li>test2</li>
-          <li>aboyt</li>
+          <li>Home</li>
+          <li>About</li>
+          <li>Test</li>
+          <li>Test2</li>
         </ul>
-        <input type='text' placeholder='search' />
+        <input type='text' placeholder='Search' />
       </div>
-      <div className='grid grid-cols-3 gap-[30px] sm:grid-cols-2 xs:grid-cols-1'>
-        <Collection
-          name='travel around woerd'
+
+      {/* Colection */}
+      <div className='grid grid-cols-1 gap-[30px] mt-[40px] sm:grid-cols-2'>
+        {/* <Collection
+          name='Travel Around World'
           photos={[
-            'https://images.unsplash.com/photo-1616578738046-8d6bbb4ee28e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGFyY2hpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1629774631753-88f827bf6447?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGFyY2hpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1609618486540-8b210e32fe79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fGFyY2hpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1608507777998-7b4f2c269120?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fGFyY2hpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1613310023042-ad79320c00ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bmF0aW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1612676239016-41e2c92b8e06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW91bmF0aW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1610809027249-86c649feacd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bW91bmF0aW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1621682372775-533449e550ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bW91bmF0aW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
           ]}
-        />
+        /> */}
+
+        {collections.map((colection) => (
+          <Collection
+            key={colection.name}
+            name={colection.name}
+            photos={colection.photos}
+          />
+        ))}
       </div>
     </div>
   );
--- components/Collection.tsx
+++ components/Collection.tsx
@@ -1,49 +1,77 @@
-'use clients';
+'use client';
 import Image from 'next/image';
 
-type colectiontype = {
+type CollectionProps = {
   name: string;
   photos: string[];
 };
 
-export default function Collection({ name, photos }: colectiontype) {
+export default function Collection({ name, photos }: CollectionProps) {
   return (
-    <div className='max-w-[460px]  bg-white p-[30px] rounded-[30px] shadow-md cursor-pointer transform duration-150 ease in out hover:shadow-xl hover:transform hover:translate-x-[5px]'>
-      <Image
-        className='h-250px w-ful object fit rounded mg-15'
-        width={470}
-        height={250}
-        objectFit='cover'
-        alt='item'
-        src={photos[1]}
-      ></Image>
+    <div className='max-w-[470px] bg-white p-[30px] rounded-[30px] shadow-md cursor-pointer transition-all duration-150 ease-in-out hover:shadow-xl hover:transform hover:translate-y-[-5px] '>
+      {photos[0] ? (
+        <Image
+          className='h-[250px] w-full object-cover rounded-[20px] mb-[15px]'
+          width={470}
+          height={250}
+          objectFit='cover'
+          alt='Item'
+          src={photos[0]}
+        />
+      ) : (
+        <div className='h-[250px] w-full object-cover rounded-[20px] mb-[15px] bg-gray-300'></div>
+      )}
+
+      {photos[0] ? (
+        <Image
+          className='h-[250px] w-full object-cover rounded-[20px] mb-[15px]'
+          width={470}
+          height={250}
+          objectFit='cover'
+          alt='Item'
+          src={photos[0]}
+        />
+      ) : (
+        <div className='h-[250px] w-full object-cover rounded-[20px] mb-[15px] bg-gray-300'></div>
+      )}
+
       <div className='flex justify-between'>
-        <Image
-          className='w-[31%] h-80pc rounded-[20px'
-          width={470}
-          height={250}
-          objectFit='cover'
-          alt='item'
-          src={photos[2]}
-        ></Image>
-
-        <Image
-          className='w-[31%] h-80pc rounded-[20px'
-          width={470}
-          height={250}
-          objectFit='cover'
-          alt='item'
-          src={photos[3]}
-        ></Image>
-
-        <Image
-          className='w-[31%] h-80pc rounded-[20px'
-          width={470}
-          height={250}
-          objectFit='cover'
-          alt='item'
-          src={photos[4]}
-        ></Image>
+        {photos[0] ? (
+          <Image
+            className='w-[31%] h-[80px] rounded-[20px]'
+            width={470}
+            height={250}
+            objectFit='cover'
+            alt='Item'
+            src={photos[1]}
+          />
+        ) : (
+          <div className='h-[250px] w-full object-cover rounded-[20px] mb-[15px] bg-gray-300'></div>
+        )}
+        {photos[0] ? (
+          <Image
+            className='w-[31%] h-[80px] rounded-[20px]'
+            width={470}
+            height={250}
+            objectFit='cover'
+            alt='Item'
+            src={photos[2]}
+          />
+        ) : (
+          <div className='h-[250px] w-full object-cover rounded-[20px] mb-[15px] bg-gray-300'></div>
+        )}
+        {photos[0] ? (
+          <Image
+            className='w-[31%] h-[80px] rounded-[20px]'
+            width={470}
+            height={250}
+            objectFit='cover'
+            alt='Item'
+            src={photos[3]}
+          />
+        ) : (
+          <div className='h-[250px] w-full object-cover rounded-[20px] mb-[15px] bg-gray-300'></div>
+        )}
       </div>
     </div>
   );
